70d994cea46770d24bd80207859a670a5fc035a1
Irina Arinushkina <ira.arinushkina@gmail.com>
2018-02-18 20:03:42 +0300

    Тесты

---
+++ src/generator/data.js
@@ -0,0 +1,50 @@
+module.exports = {
+  TITLES: [
+    `Большая уютная квартира`,
+    `Маленькая неуютная квартира`,
+    `Огромный прекрасный дворец`,
+    `Маленький ужасный дворец`,
+    `Красивый гостевой домик`,
+    `Некрасивый негостеприимный домик`,
+    `Уютное бунгало далеко от моря`,
+    `Неуютное бунгало по колено в воде`
+  ],
+  TYPES: [
+    `flat`,
+    `palace`,
+    `house`,
+    `bungalo`
+  ],
+  CHECKIN_TIMES: [
+    `12:00`,
+    `13:00`,
+    `14:00`
+  ],
+  CHECKOUT_TIMES: [
+    `12:00`,
+    `13:00`,
+    `14:00`
+  ],
+  FEATURES: [
+    `wifi`,
+    `dishwasher`,
+    `parking`,
+    `washer`,
+    `elevator`,
+    `conditioner`
+  ],
+  PHOTOS: [
+    `http://o0.github.io/assets/images/tokyo/hotel1.jpg`,
+    `http://o0.github.io/assets/images/tokyo/hotel2.jpg`,
+    `http://o0.github.io/assets/images/tokyo/hotel3.jpg`
+  ],
+  PRICE: {
+    MIN: 1000,
+    MAX: 1000000
+  },
+  ROOMS: {
+    MIN: 1,
+    MAX: 5
+  }
+
+};
