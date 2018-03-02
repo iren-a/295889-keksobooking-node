24b3aa08568e319d00cf529f37e94912f4650104
Irina Arinushkina <ira.arinushkina@gmail.com>
2018-03-02 17:42:56 +0300

    #12 Прибытие поезда на вокзал Ла Сьота

---
+++ test/get-offers.test.js
@@ -0,0 +1,60 @@
+const request = require(`supertest`);
+const assert = require(`assert`);
+const {app} = require(`../src/server`);
+
+describe(`GET /api/offers`, function () {
+
+  it(`respond to request without query`, () => {
+    return request(app)
+        .get(`/api/offers`)
+        .set(`Accept`, `application/json`)
+        .expect(200)
+        .expect(`Content-Type`, /json/)
+        .then((response) => {
+          const offers = response.body;
+          assert.equal(offers.data.length, 20);
+          assert.equal(Object.keys(offers.data[0]).length, 4);
+        });
+  });
+
+  it(`respond to request with correct query`, () => {
+    return request(app)
+        .get(`/api/offers?skip=5&limit=3`)
+        .set(`Accept`, `application/json`)
+        .expect(200)
+        .expect(`Content-Type`, /json/)
+        .then((response) => {
+          const offers = response.body;
+          assert.equal(offers.data.length, 3);
+        });
+  });
+
+  it(`by incorrect query should respond with 400`, () => {
+    return request(app)
+        .get(`/api/offers?skip=incorrect&limit=incorrect`)
+        .set(`Accept`, `application/json`)
+        .expect(400)
+        .expect(`Content-Type`, /html/);
+  });
+
+  it(`find offer by date`, () => {
+    return request(app)
+        .get(`/api/offers/${encodeURIComponent(+new Date(2018, 1))}`)
+        .expect(200)
+        .expect(`Content-Type`, /json/)
+        .then((response) => {
+          const offer = response.body;
+          assert.equal(offer.date, +new Date(2018, 1));
+        });
+  });
+
+  it(`unknown address should respond with 404`, () => {
+    return request(app)
+        .get(`/api/unknown`)
+        .set(`Accept`, `application/json`)
+        .expect(404)
+        .expect(`Content-Type`, /html/);
+  });
+
+
+});
