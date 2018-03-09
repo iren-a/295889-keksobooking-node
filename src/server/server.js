0383842874b1171c536345278f5461ab7a6354fc
Irina Arinushkina <ira.arinushkina@gmail.com>
2018-03-09 20:05:29 +0300

    #16 Хлебные крошки

--- src/server/server.js
+++ src/server/server.js
@@ -2,9 +2,10 @@ const express = require(`express`);
 const offerStore = require(`./offers/store`);
 const imageStore = require(`./images/store`);
 const offersRouter = require(`./offers/route`)(offerStore, imageStore);
+const logger = require(`../logger`);
 
-const HOSTNAME = `127.0.0.1`;
-const PORT = 3000;
+const HOSTNAME = process.env.SERVER_HOST || `localhost`;
+const PORT = parseInt(process.env.SERVER_PORT, 10) || 3000;
 
 const app = express();
 app.use(express.static(`static`));
@@ -14,7 +15,7 @@ app.use(`/api/offers`, offersRouter);
 module.exports = {
   run(port = PORT) {
     app.listen(port, HOSTNAME, () => {
-      console.log(`Server running at http://${HOSTNAME}:${port}`);
+      logger.info(`Server running at http://${HOSTNAME}:${port}`);
     });
   },
   app
--- src/server/util/data-renderer.js
+++ src/server/util/data-renderer.js
@@ -1,5 +1,6 @@
 const util = require(`util`);
 const ValidationError = require(`../error/validation-error`);
+const {MongoError} = require(`mongodb`);
 
 const SUCCESS_CODE = 200;
 const BAD_DATA_CODE = 400;
@@ -45,6 +46,7 @@ ${util.inspect(form)}
 const render = (req, res, data, success) => {
   const badStatusCode = data.code ? data.code : BAD_DATA_CODE;
   res.status(success ? SUCCESS_CODE : badStatusCode);
+
   switch (req.accepts([`json`, `html`])) {
     case `html`:
       res.set(`Content-Type`, `text/html`);
@@ -63,6 +65,17 @@ module.exports = {
     let data = exception;
     if (exception instanceof ValidationError) {
       data = exception.errors;
+    } else if (exception instanceof MongoError) {
+      data = {};
+      switch (exception.code) {
+        case 11000:
+          data.code = 400;
+          data.errorMessage = `Дубликат существующего объявления`;
+          break;
+        default:
+          data.code = 501;
+          data.errorMessage = exception.message;
+      }
     }
     render(req, res, data, false);
   }
